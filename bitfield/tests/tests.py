cde12bbe093dc1c71b006ac383e3c4d5b5f7defb
Stephen Goodman <stephen.goodman@gmail.com>
2016-06-23 08:21:45 -0500

    changed depreciated function

--- bitfield/tests/tests.py
+++ bitfield/tests/tests.py
@@ -171,7 +171,7 @@ class BitFieldTest(TestCase):
         self.assertTrue(instance.flags.FLAG_3)
 
         cursor = connection.cursor()
-        flags_field = BitFieldTestModel._meta.get_field_by_name('flags')[0]
+        flags_field = BitFieldTestModel._meta.get_field('flags')
         flags_db_column = flags_field.db_column or flags_field.name
         cursor.execute("INSERT INTO %s (%s) VALUES (-1)" % (BitFieldTestModel._meta.db_table, flags_db_column))
         # There should only be the one row we inserted through the cursor.
